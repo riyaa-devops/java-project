c2b5d3bee1b477299225dc813c3de35fd8006c1b
riyaa-devops <riyaseetharam@gmail.com>
2026-04-16 14:23:43 +0530

    Add files via upload

---
+++ devops/Demo1.java
@@ -0,0 +1,8 @@
+class Demo {
+    int volume = 100; 
+
+    public static void main(String args[]) {
+        Demo d1 = new Demo();
+        System.out.println(d1.volume);
+    }
+}
---
+++ devops/j1.java
@@ -0,0 +1,11 @@
+class Demo {
+    // You must specify the data type (int)
+    int volume = 100; 
+}
+
+class Demo1 {
+    public static void main(String args[]) {
+        Demo d1 = new Demo();
+        System.out.println(d1.volume);
+    }
+}
