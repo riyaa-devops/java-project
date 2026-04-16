7f869bcd3315463eef6b36ee2d25f936964bce7e
riyaa-devops <riyaseetharam@gmail.com>
2026-04-17 00:49:03 +0530

    Rename class Demo1 to j1 in j1.javachanged file name

--- devops/j1.java
+++ devops/j1.java
@@ -3,7 +3,7 @@ class Demo {
     int volume = 100; 
 }
 
-class Demo1 {
+class j1 {
     public static void main(String args[]) {
         Demo d1 = new Demo();
         System.out.println(d1.volume);
